f173a5cd18e5808c818f2047e0ce23b65e68a5b3
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-18 16:22:54 -0500

    Se creo las función para darle movimiento a las barras (Bar) pero, aún no se dibujan

--- main.js
+++ main.js
@@ -26,14 +26,18 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: "+ this.x + " y: "+ this.y; 
         }
     }
 
@@ -71,14 +75,26 @@
     }
 })();
 
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100, board);
+var bar2 = new Bar(735, 100, 40, 100, board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+
+
+document.addEventListener("keydown", function(ev){
+    if(ev.keyCode == 38){
+        bar.up();
+    }else if(ev.keyCode == 40){
+        bar.down();
+    }
+
+    console.log(""+bar);
+});
+
 self.addEventListener("load", main);
 
 function main(){
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100, board);
-    var bar2 = new Bar(735, 100, 40, 100, board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas, board);
-
+    
     board_view.draw();
 }
