e69729c7560f513be888592235fe26e44ed51f01
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-18 17:27:26 -0500

    Se agrega el movimiento de la pelota, se solucionaron bugs de version anterior, se agrego la funcionalidad de iniciar el juego presionando una tecla (space)

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
+
+//Función que se ejecuta automaticamente y dentro contiene la clase Board (tablero).
 (function(){
+    //Creamos la clase
     self.Board = function(width, height){
+        //asignamos los datos a los atributos de la clase
         this.width = width;
         this.height = height;
         this.playing = false;
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
     
+    //Prototype de la clase
     self.Board.prototype = {
+        //Metodos de la clase
         get elements(){
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;});
             elements.push(this.ball);
             return elements;
         }
     }
 })();
 
+//Función automatica
 (function(){
+    //Clase Ball (pelota)
     self.Ball = function(x, y, radius, board){
+        //Asignamos los datos a los atributos de la clase
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
+
         board.ball = this;
         this.kind = "circle";
     }
+    
+    //Prototype de la clase Ball
+    self.Ball.prototype = {
+        //Metodos de la clase Ball
+        move: function(){ //Mueve la pelota
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+            
+        }
+    }
 })();
 
+//Función automatica
 (function(){
+    //Clase Bar (Barra)
     self.Bar = function(x, y, width, height, board){
+        //Asignar datos a los atributos de la clase
         this.x = x;
         this.y = y;
         this.width = width;
@@ -42,23 +67,28 @@
         this.speed = 10;
     }
 
+    //Protytpe de Bar
     self.Bar.prototype = {
-        down: function(){
+        //Metodos de Bar
+        down: function(){ //Mueve la barra hacia abajo
             this.y += this.speed;
         },
-        up: function(){
+        up: function(){ //Mueve la barra hacia arriba
             this.y -= this.speed;
         },
-        toString: function(){
+        /*toString: function(){ //La utlizamos para testear y ver si funcionaba
             return "x: "+ this.x + " y: "+ this.y; 
-        }
+        }*/
     }
 
 }
 )();
 
+//Función automatica
 (function(){
+    //Clase BoardView (Ver Tablero)
     self.BoardView = function(canvas, board){
+        //Se asigna los datos a los atributos de la clase
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
@@ -66,23 +96,29 @@
         this.ctx = canvas.getContext("2d");
     }
 
+    //Prototype de BoardView
     self.BoardView.prototype = {
-        clean: function(){
+        //Metodos de la clase
+        clean: function(){ //Limpia cada objeto dentro del Board para dibujarlo de nuevo a donde se quiere mover
             this.ctx.clearRect(0,0,this.board.width, this.board.height);
         },
-        draw: function(){
+        draw: function(){ //Dibuja todo 1 vez
             for(var i = this.board.elements.length - 1; i>=0; i--){
                 var el = this.board.elements[i];
 
                 draw(this.ctx, el);
             }
         },
-        play: function(){
-            this.clean();
-            this.draw();
+        play: function(){ //Metodo para controlar el dibujado (draw) y la limpieza de los objetos (clean)
+            if(this.board.playing){
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
+    //Función para saber que dibujar (rectangulo, circulo, etc);
     function draw(ctx, element){
         switch(element.kind){
             case "rectangle":
@@ -98,6 +134,7 @@
     }
 })();
 
+//Objetos de las clases utilizadas, con sus datos de entrada
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100, board);
 var bar2 = new Bar(735, 100, 40, 100, board);
@@ -105,22 +142,33 @@ var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
-
-window.requestAnimationFrame(controller);
-
+//Recibimos por el document las teclas presionadas y con estás efectuamos las acciones en la pantalla (dibujamos).
 document.addEventListener("keydown", function(ev){
-    ev.preventDefault();
-    if(ev.keyCode == 87){
+    if(ev.keyCode == 87){ //Tecla W
+        ev.preventDefault();
         bar.up();
-    }else if(ev.keyCode == 83){
+    }else if(ev.keyCode == 83){ //Tecla S
+        ev.preventDefault();
         bar.down();
-    }else if(ev.keyCode == 38){
+    }else if(ev.keyCode == 38){ //Tecla Flecha arriba
+        ev.preventDefault();
         bar2.up();
-    }else if(ev.keyCode == 40){
+    }else if(ev.keyCode == 40){ //Tecla flecha abajo
+        ev.preventDefault();
         bar2.down();
+    }else if(ev.keyCode == 32){ //Tecla espacio
+        ev.preventDefault();
+        board.playing = !board.playing; //Parar o continuar el juego (dibujo).
     }
 });
 
+//Dibujamos una vez al iniciar para ver algo y no necesariamente presionar espacio para comenzar.
+board_view.draw();
+//Controlar el dibujado de la pantalla por la función controller
+window.requestAnimationFrame(controller);
+/*setTimeout(function(){ //Es para testear el movimiento de la Ball (pelota).
+    ball.direction = -1;
+}, 4000)*/
 function controller(){
     board_view.play();
     window.requestAnimationFrame(controller);
